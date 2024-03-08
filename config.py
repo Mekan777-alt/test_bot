94c9abc9cb1baca6e76804aa69ae3af294209f31
Mekan777-alt <mekan2678@gmail.com>
2024-03-08 11:10:36 +0300

    update port redis

--- config.py
+++ config.py
@@ -30,4 +30,4 @@ bot = Bot(token=token)
 
 dp = Dispatcher(bot=bot, storage=MemoryStorage())
 
-redis_client = redis.Redis(host='redis', port=6380, db=0)
+redis_client = redis.Redis(host='redis', port=6379, db=0)
